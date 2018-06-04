6f93f4df962aa52e3372ff2efbdb1d5c9cbe5132
chending <cdchenidng@126.com>
2018-06-04 19:55:58 +0800

    用户列表表格

--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -18,11 +18,79 @@
         <el-button type="success" plain>添加用户</el-button>
       </div></el-col>
     </el-row>
+    <!-- 表格 -->
+     <el-table
+      stripe
+      border
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="primary"
+            icon="el-icon-edit"
+            plain>
+          </el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            icon="el-icon-delete"
+            plain>
+          </el-button>
+          <el-button
+            type="success"
+            icon="el-icon-check"
+            size="mini"
+            plain>
+          </el-button>
+        </template>
+      </el-table-column>
+     </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }]
+        }
+    }
 };
 </script>
 
@@ -31,9 +99,12 @@ export default {
     width: 100%;
     height: 100%;
   }
+  
   .searchContainer {
     margin-top: 15px;
+    margin-bottom: 15px;
   }
+
   .searchInput {
     width: 300px;
   }
