572c3ae250111b71d903132e99bc7fb3c0f7a6d1
chending <cdchenidng@126.com>
2018-06-06 14:27:10 +0800

    '修改页面'

--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -69,6 +69,7 @@
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button
+            @click="handleOpenEditDialog(scope.row)"
             size="mini"
             type="primary"
             icon="el-icon-edit"
@@ -127,6 +128,26 @@
         <el-button type="primary" @click="handleAdd">确 定</el-button>
       </div>
     </el-dialog>
+    <!-- 编辑用户的对话框 -->
+    <el-dialog title="" :visible.sync="editUserDialogVisible">
+      <el-form
+      label-width="100px"
+      :model="userFormData">
+        <el-form-item label="用户名">
+          <el-input v-model="userFormData.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="userFormData.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="userFormData.name" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button>取 消</el-button>
+        <el-button type="primary">确 定</el-button>
+      </div>
+</el-dialog>
   </el-card>
 </template>
 
@@ -138,28 +159,30 @@ export default {
       pagenum: 1,
       pagesize: 4,
       total: 0,
-      searchKey: '',
+      searchKey: "",
       // 控制添加用户的对话框是否显示
       addUserDialogVisible: false,
       // 用户对应的表单数据
       userFormData: {
-        username: '',
-        password: '',
-        email: '',
-        mobile: ''
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
       },
       // 控制加载提示显示隐藏
       loading: true,
       rules: {
         username: [
-          { require: true, message: '请输入用户名', trigger: 'blur' },
-          { min:3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur'}
+          { require: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "blur" }
         ],
         password: [
-          { require: true, message: '请输入密码', trigger: 'blur' },
-          { min:5, max: 11, message: '长度在 5 到 11 个字符', trigger: 'blur'}
+          { require: true, message: "请输入密码", trigger: "blur" },
+          { min: 5, max: 11, message: "长度在 5 到 11 个字符", trigger: "blur" }
         ]
-      }
+      },
+      // 控制编辑窗口显示隐藏
+      editUserDialogVisible: false
     };
   },
   created() {
@@ -167,42 +190,48 @@ export default {
     this.loadData();
   },
   methods: {
+    // 点击编辑按钮，打开修改用户的对话框，并且把当前用户信息显示
+    handleOpenEditDialog(user) {
+      this.editUserDialogVisible = true;
+      this.userFormData = user;
+      console.log(this.userFormData);
+    },
     async handleAdd() {
-      const { data } = await this.$http.post('users',this.userFormData);
-      if(data.meta.status === 201) {
+      const { data } = await this.$http.post("users", this.userFormData);
+      if (data.meta.status === 201) {
         // 添加成功
-        this.$message.success('添加成功');
+        this.$message.success("添加成功");
         this.loadData();
         // 关闭对话框
         this.addUserDialogVisible = false;
         // 清空文本框中的值
-        for(let key in this.userFormData) {
-          this.userFormData[key] = '';
+        for (let key in this.userFormData) {
+          this.userFormData[key] = "";
         }
-      }else {
+      } else {
         this.$message.error(data.meta.msg);
       }
     },
     // 根据id删除用户
     async handleDel(id) {
-      this.$confirm('是否确定删除该用户?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(async () => {
-          // 删除操作
-          const { data } = await this.$http.delete(`users/${id}`);
-          if (data.meta.status === 200) {
-            // 删除成功
-            this.$message.success('删除成功');
-            this.pagenum = 1;
-            // 重新加载数据
-            this.loadData();
-          } else {
-            // 删除失败
-            this.$message.error(data.meta.msg);
-          }
-        });
+      this.$confirm("是否确定删除该用户?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(async () => {
+        // 删除操作
+        const { data } = await this.$http.delete(`users/${id}`);
+        if (data.meta.status === 200) {
+          // 删除成功
+          this.$message.success("删除成功");
+          this.pagenum = 1;
+          // 重新加载数据
+          this.loadData();
+        } else {
+          // 删除失败
+          this.$message.error(data.meta.msg);
+        }
+      });
     },
     // 开个状态改变
     async handleChange(user) {
@@ -212,9 +241,9 @@ export default {
       );
       const data = res.data;
       if (data.meta.status === 200) {
-        this.$message.success('用户状态修改成功');
+        this.$message.success("用户状态修改成功");
       } else {
-        this.$message.error('用户状态修改失败');
+        this.$message.error("用户状态修改失败");
       }
     },
     // 搜索功能
@@ -238,9 +267,9 @@ export default {
     async loadData() {
       this.loading = true;
       // 获取登录后的token
-      const token = sessionStorage.getItem('token');
+      const token = sessionStorage.getItem("token");
       // axios发送请求的时候需要携带token
-      this.$http.defaults.headers.common['Authorization'] = token;
+      this.$http.defaults.headers.common["Authorization"] = token;
       // 发送请求
       //   const res = await this.$http.get('users?pagenum=1&pagesize=10');
       const res = await this.$http.get(
@@ -249,15 +278,15 @@ export default {
         }`
       );
       setTimeout(() => {
-        this.loading = false
-      },1000)
+        this.loading = false;
+      }, 1000);
       // 获取服务器返回的数据
       const data = res.data;
       if (data.meta.status === 200) {
         this.total = data.data.total;
         this.tableData = data.data.users;
       } else {
-        this.$message.error('获取数据失败');
+        this.$message.error("获取数据失败");
       }
     }
   }
